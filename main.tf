15d8e3aee5ece7c3d4ece4acd68d9f7de2c08838
Rahulbobadi <rahul.bobadi@gmail.com>
2026-06-10 14:59:24 +0530

    upated secrets veriables

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "aks-gitops-rg1"
+  name     = "aks-gitops-rg2"
   location = "Central India"
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "aksgitopsacr12345"
+  name                = "aksgitopsacr123"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
