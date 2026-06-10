722c434efeeb44b794caf361da6ad9aea13aa48c
Rahulbobadi <rahul.bobadi@gmail.com>
2026-06-10 15:25:04 +0530

    modified main file and added destroy yaml

--- main.tf
+++ main.tf
@@ -31,10 +31,3 @@ resource "azurerm_kubernetes_cluster" "aks" {
     environment = "demo"
   }
 }
-
-resource "azurerm_role_assignment" "acr_pull" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-}
